df56d8fadfac1c269e949f1b7d5e0d390c931fe0
𝐃𝐀𝐑𝐑𝐄𝐋𝐋 𝐌𝐔𝐂𝐇𝐄𝐑𝐈 ⚡ <darrellmucheri@yahoo.com>
2025-01-24 23:29:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -15,6 +15,7 @@ Repo         : https//github.com/mrfrank-ofc/SUBZERO-MD
 Support      : wa.me/18062212660
 */
 
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -22,94 +23,101 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "Put Your Session ID Here",
+// add your Session Id 
+ 
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+// make true or false status auto seen
+
+ 
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+// make true if you want auto reply on status 
+ 
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*υℓτιмατє ѕυϐzєяο м∂ ϐοτ νιєωє∂ γουя ѕτατυѕ ❄*",
+// set the auto reply massage on status reply  
  
 PREFIX: process.env.PREFIX || ".",
-// Enter Your Desired Prefix
-
-BOT_NAME: process.env.BOT_NAME || "『 𝐒𝐔𝐁𝐙𝐄𝐑𝐎 𝐌𝐃 』",
-// Enter Your Bot Name
-
-ANTI_VV: process.env.ANTI_VV || "true",
-// Allow Viewoncs view Unlimited 
+// add your prifix for bot   
  
+BOT_NAME: process.env.BOT_NAME || "SUBZERO-MD",
+// add bot namw here for menu
+ 
+STICKER_NAME: process.env.STICKER_NAME || "ѕυϐzєяο-м∂-ϐοτ",
+// type sticker pack name 
+
 CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react  
-  
+// make this true for custum emoji react    
+
+ 
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
 // chose custom react emojis by yourself 
 
-DELETE_LINKS: process.env.DELETE_LINKS || "true",
+ 
+DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "263719647303",
-// Set Owner Name
+ OWNER_NUMBER: process.env.OWNER_NUMBER || "263719×××",
+// add your bot owner number
 
-OWNER_NAME: process.env.OWNER_NAME || "ᴍʀ ꜰʀᴀɴᴋ ᴏꜰᴄ",
-// Set Footer
+ OWNER_NAME: process.env.OWNER_NAME || "Mr Frank OFC",
+// add bot owner name
 
-DESCRIPTION: process.env.DESCRIPTION || "*© Gᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Sᴜʙᴢᴇʀᴏ*",
-// add bot owner name   
- 
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/YkJyMRb/mrfrankofc.jpg",
+ DESCRIPTION: process.env.DESCRIPTION || "*© Gᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Sᴜʙᴢᴇʀᴏ*",
+// add bot owner name    
+
+ ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/v1nC1PP/mrfrankofc.jpg",
 // add img for alive msg
 
-LIVE_MSG: process.env.LIVE_MSG || "> [🎐] SᴜʙZᴇʀᴏ MD ɪs ᴏɴʟɪɴᴇ*⚡",
-// add alive msg here 
-
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
+ READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
 
-AUTO_REACT: process.env.AUTO_REACT || "false",
+ AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 
-ANTI_BAD: process.env.ANTI_BAD || "true",
-// false or true for anti bad words
+ ANTI_BAD: process.env.ANTI_BAD || "true",
+// false or true for anti bad words  
 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
-// make true or false status auto seen
-
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-
-AUTO_STATUS_MSG: process.env.AUTO_STATUS__MSG || "*[❄️] Hi there, SUBZERO-MD viewed your Status🎐*",
-
-// set the auto reply massage on status reply    
-MODE: process.env.MODE || "public",
+ MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
 
-ANTI_LINK: process.env.ANTI_LINK || "true",
+ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+ AUTO_VOICE: process.env.AUTO_VOICE || "false",
 // make true for send automatic voices
 
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
+ AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
 
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
-// make true or false automatic text reply
- 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+ AUTO_REPLY: process.env.AUTO_REPLY || "true",
+// make true or false automatic text reply 
+
+ ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+ PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
 // make false if want private mod
 
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing  
- 
-READ_CMD: process.env.READ_CMD || "true",
-// true if want mark commands as read 
+ AUTO_TYPING: process.env.AUTO_TYPING || "false",
+// true for automatic show typing   
 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
+ DEV: process.env.DEV || "263719647303",
+//replace with your whatsapp number        
+
+ ANTI_VV: process.env.ANTI_VV || "true",
+// true for anti once view 
+
+ ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", // change it to 'same' if you want to resend deleted message in same chat 
+
+ AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
 // make it true for auto recoding 
 };
 
-/*
 
 
-_____/\\\\\\\\\\\____/\\\________/\\\__/\\\\\\\\\\\\\____/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\____/\\\\\\\\\___________/\\\\\_________________/\\\\____________/\\\\__/\\\\\\\\\\\\____        
+
+
+/*_____/\\\\\\\\\\\____/\\\________/\\\__/\\\\\\\\\\\\\____/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\____/\\\\\\\\\___________/\\\\\_________________/\\\\____________/\\\\__/\\\\\\\\\\\\____        
  ___/\\\/////////\\\_\/\\\_______\/\\\_\/\\\/////////\\\_\////////////\\\__\/\\\///////////___/\\\///////\\\_______/\\\///\\\______________\/\\\\\\________/\\\\\\_\/\\\////////\\\__       
   __\//\\\______\///__\/\\\_______\/\\\_\/\\\_______\/\\\___________/\\\/___\/\\\_____________\/\\\_____\/\\\_____/\\\/__\///\\\____________\/\\\//\\\____/\\\//\\\_\/\\\______\//\\\_      
    ___\////\\\_________\/\\\_______\/\\\_\/\\\\\\\\\\\\\\__________/\\\/_____\/\\\\\\\\\\\_____\/\\\\\\\\\\\/_____/\\\______\//\\\___________\/\\\\///\\\/\\\/_\/\\\_\/\\\_______\/\\\_     
